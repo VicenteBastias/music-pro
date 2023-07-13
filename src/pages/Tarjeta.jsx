02759bffdc7230a52ee94caf25e7b9bae8df2468
Vicente <vice.bastias@duocuc.cl>
2023-07-13 10:22:04 -0400

    historial

--- src/pages/Tarjeta.jsx
+++ src/pages/Tarjeta.jsx
@@ -13,6 +13,7 @@ function Tarjeta() {
     const id_usuario = urlParams.get('id')
     console.log(id_usuario)
     const[Tarjeta, setTarjeta] = useState([{}])
+    const[Historial, setHistorial] = useState([{}])
     const handleClick = (e) => {
         const saldo = inputSaldo.current.value
         fetch(`http://localhost:5000/TarjetaSaldo?monto=${saldo}&id_usuario='${id_usuario}'`)
@@ -31,6 +32,17 @@ function Tarjeta() {
             setTarjeta(getdata);
             }
         }
+        const getHistorial = async ()=> {
+            if (id_usuario === ""){
+                navigate("/login")
+                return alert("Inicie sesion para revisar su tarjeta")
+            }else{
+            const res = await fetch(`http://localhost:5000/Historial?id_usuario='${id_usuario}'`);
+            const getdata = await res.json();
+            setHistorial(getdata);
+            }
+        }
+        getHistorial()
         getTarjeta();
     },[]);    
         
@@ -65,6 +77,31 @@ function Tarjeta() {
         </div>
         <div>
             <button type="button" class="btn btn-primary btn-block mb-4"onClick={navigateTransferencia}>Transferencia</button>
+        </div>
+        <div>
+        <table className="table">
+            <thead>
+                <tr>
+                <th scope="col">Id Transferencia</th>
+                <th scope="col">Tarjeta Destino</th>
+                <th scope="col">Comentario</th>
+                <th scope="col">Monto</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    Historial.map( (getHistorial) =>(
+                       <tr> 
+                        <td>{ getHistorial.id_transferencia}</td>
+                        <td>{ getHistorial.tarjeta_destino}</td>
+                        <td>{ getHistorial.comentario}</td>
+                        <td>{ getHistorial.monto}</td>
+                        </tr>
+                    )
+                    )
+                }
+            </tbody>
+            </table>
         </div>
     </div>
 
